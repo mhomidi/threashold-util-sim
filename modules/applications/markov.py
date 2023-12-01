0420ee080d701f47ce0b1a9d6422b02ec0bc27fc
smajidzahedi <smajid.zahedi@yahoo.com>
2023-12-01 15:03:18 -0500

    new markov

--- modules/applications/markov.py
+++ modules/applications/markov.py
@@ -2,54 +2,23 @@
 
 from modules.applications import Application, State
 import utils
+import numpy as np
 import random
 
 
 class MarkovApplication(Application):
 
-    def __init__(self) -> None:
+    def __init__(self, utilities, transition_matrix, initial_index) -> None:
         super(MarkovApplication, self).__init__()
-        self.transitions = dict()
-
-    def add_state(self, state: State) -> None:
-        if self.init_state is None:
-            self.init_state = state
-            self.curr_state = state
-        self.transitions[state] = dict()
-        self.states.append(state)
-
-    def add_transition(self, src: State, dest: State, prob: float) -> None:
-        self.transitions[src][dest] = prob
+        self.utilities = utilities
+        self.len_utilities = len(utilities)
+        self.transition_matrix = transition_matrix
+        self.curr_state = self.utilities[initial_index]
 
     def go_next_state(self) -> None:
-        trans = self.transitions[self.curr_state]
-        rand = random.random()
-        start_point = 0.0
-        selected_state = None
-        for state in trans:
-            start_point += trans[state]
-            if rand < start_point:
-                selected_state = state
-                break
-        if selected_state is None:
-            raise Exception()
-        self.curr_state = selected_state
+        current_index = self.utilities.index(self.curr_state)
+        trans = self.transition_matrix[current_index]
+        self.curr_state = np.random.choice(self.utilities, p=trans)
 
-    def init_from_json(self, json_file: str) -> None:
-        data = utils.get_json_data_from_file(json_file)
-        for key in data:
-            ru = State(key, data[key]["utils"])
-            self.add_state(ru)
-        for key in data:
-            src = self.get_state_with_name(key)
-            for state_name, prob in data[key]["transitions"].items():
-                dest = self.get_state_with_name(state_name)
-                self.add_transition(src, dest, prob)
-
-    # def get_mean_util(self):
-    #     mean = 0.
-    #     for key in self.transitions:
-    #         key: State
-    #         u = sum(key.get_utils()) / len(key.get_utils())
-    #         mean += u * key.get_prob()
-    #     return mean
+    def get_utility(self):
+        return self.curr_state
--- modules/applications/queue.py
+++ modules/applications/queue.py
@@ -1,7 +1,5 @@
-
 from modules.applications import Application, State
 from utils.distribution import *
-import random
 import numpy as np
 
 
@@ -9,34 +7,29 @@ class QueueApplication(Application):
 
     def __init__(self, throughput, max_queue_length, initial_queue_length) -> None:
         super().__init__()
-        self.cluster_range = range(config.get(config.CLUSTER_NUM))
+        self.num_clusters = config.get(config.CLUSTER_NUM)
         self.init_state = State(name="init", val=initial_queue_length)
         self.curr_queue_lengths = np.array(self.init_state.val)
         self.max_queue_length = max_queue_length
         self.curr_state = np.maximum(self.max_queue_length, self.curr_queue_lengths)
-        self.departure_generators = [PoissonGenerator(throughput[i]) for i in self.cluster_range]
+        self.departure_generators = [PoissonGenerator(throughput[i]) for i in range(0, self.num_clusters)]
+        self.arrivals = np.zeros(self.num_clusters)
+        self.allocations = np.zeros(self.num_clusters)
 
-    def go_next_state(self, arrivals, allocations) -> None:
-        # self.curr_state.set_val([self.arrival_generator.generate()])
-        # self.max_queue_length += self.init_state.val[0]
+    def set_arrival(self, arrivals):
+        self.arrivals = arrivals
 
-        # TODO: do it by numpy all
+    def set_allocation(self, allocations):
+        self.allocations = allocations
+
+    def go_next_state(self):
         next_departures = np.array([a.generate() for a in self.departure_generators])
-        self.curr_queue_lengths = np.minimum(self.curr_queue_lengths + arrivals - allocations * next_departures, 0)
+        self.curr_queue_lengths = np.minimum(self.curr_queue_lengths + self.arrivals -
+                                             self.allocations * next_departures, 0)
         self.curr_state = np.maximum(self.max_queue_length, self.curr_queue_lengths)
     
-    def get_current_queue_lengths(self) -> list:
+    def get_current_queue_lengths(self):
         return self.curr_queue_lengths
 
-    # TODO: remove this
-    def reduce_length(self, passed_job: int):
-        self.max_queue_length -= passed_job
-        if self.max_queue_length < 0:
-            self.max_queue_length = 0
-
-    def get_throughput(self) -> list:
-        # TODO: -self.curr_queue_length
-        return self.throughput
-    
     def get_utilities(self):
         return -self.curr_queue_lengths
