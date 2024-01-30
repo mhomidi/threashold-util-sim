b186ad989e60c7038a5c981bbaf3483e754360b9
smajidzahedi <smajid.zahedi@yahoo.com>
2024-01-29 20:38:31 -0500

    qlengthfair + throughputfair

--- modules/scheduler/ftf_scheduler.py
+++ modules/scheduler/ftf_scheduler.py
@@ -6,7 +6,7 @@ import numpy as np
 FTF_ALPHA = 0.9
 
 
-class FinishTimeFairnessScheduler(Scheduler):
+class QLengthFairScheduler(Scheduler):
 
     def __init__(self, agent_weights, num_agents, num_clusters, departure_rates, arrival_rates):
         super().__init__(agent_weights, num_agents, num_clusters)
@@ -18,8 +18,7 @@ class FinishTimeFairnessScheduler(Scheduler):
         exclusive_utilization = self.arrival_rates / exclusive_departure_rates
         assert np.max(exclusive_utilization) < 1
         self.exclusive_q_lengths = exclusive_utilization / (1 - exclusive_utilization)
-        self.shared_queue_lengths = np.zeros((self.num_agents, self.num_clusters))
-        self.actual_arrivals = np.zeros((self.num_agents, self.num_clusters))
+        # self.shared_queue_lengths = np.zeros((self.num_agents, self.num_clusters))
 
     # TODO: for now, do not call this function anywhere
     # def update_scheduler(self, data):
@@ -36,9 +35,9 @@ class FinishTimeFairnessScheduler(Scheduler):
 
         rho = cp.Variable(1)
         x = cp.Variable((self.num_agents, self.num_clusters), boolean=True)
-        shared_departure_rates = cp.minimum(cp.multiply(x, self.departure_rates), demands)
+        # shared_departure_rates = cp.minimum(cp.multiply(x, self.departure_rates), demands)
         # shared_queue_length = FTF_ALPHA * self.shared_queue_lengths + (1 - FTF_ALPHA) * (demands - shared_departure_rates)
-        shared_queue_length = demands - shared_departure_rates
+        shared_queue_length = demands - cp.multiply(x, self.departure_rates)
 
         exc_q_lengths = ones_ac * self.exclusive_q_lengths
         constraints = [
@@ -50,6 +49,48 @@ class FinishTimeFairnessScheduler(Scheduler):
         problem.solve()
         if problem.status != cp.OPTIMAL:
             raise Exception(problem.status)
-        self.shared_queue_lengths = shared_queue_length.value
+        # self.shared_queue_lengths = shared_queue_length.value
+        allocation = np.array(x.value > 0.5, dtype=np.int8)
+        return allocation, None
+
+
+class ThroughputFairScheduler(Scheduler):
+    def __init__(self, agent_weights, num_agents, num_clusters, departure_rates, arrival_rates):
+        super().__init__(agent_weights, num_agents, num_clusters)
+        weights = self.agent_weights / self.agent_weights.sum()
+        shares = np.ones((self.num_agents, self.num_clusters)) * weights.reshape((self.num_agents, 1))
+        self.departure_rates = departure_rates
+        self.arrival_rates = arrival_rates.reshape((self.num_agents, 1))
+        exclusive_departure_rates = self.departure_rates.sum(axis=1, keepdims=True) * shares
+        self.exclusive_throughput = np.minimum(self.arrival_rates, exclusive_departure_rates)
+        ###
+        self.shared_throughput = np.zeros((self.num_agents, 1))
+        ###
+
+    def run_scheduler(self, iteration, demands) -> list:
+        ones_c = np.ones(self.num_clusters)
+        ones_a = np.ones((self.num_agents, 1))
+        ones_ac = np.ones((self.num_agents, self.num_clusters))
+
+        rho = cp.Variable(1)
+        x = cp.Variable((self.num_agents, self.num_clusters), boolean=True)
+        ###
+        shared_departures = cp.sum(cp.minimum(cp.multiply(x, self.departure_rates), demands), axis=1, keepdims=True)
+        shared_throughput = FTF_ALPHA * self.shared_throughput + (1 - FTF_ALPHA) * shared_departures
+        ###
+        # shared_throughput = shared_departure_rates
+
+        constraints = [
+            cp.sum(x, axis=0) <= ones_c,
+            rho * ones_a <= shared_throughput / self.exclusive_throughput
+        ]
+        objective = cp.Maximize(rho)
+        problem = cp.Problem(objective, constraints)
+        problem.solve()
+        if problem.status != cp.OPTIMAL:
+            raise Exception(problem.status)
+        ###
+        self.shared_throughput = shared_throughput.value
+        ###
         allocation = np.array(x.value > 0.5, dtype=np.int8)
         return allocation, None
